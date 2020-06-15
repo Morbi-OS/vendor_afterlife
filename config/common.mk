cdc3cf73c36434621a4e94486437b4a7c8839181
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-06-15 15:49:57 -0300

    vendor: Add permissions for lineagehw
    Change-Id: I55172ac4e9121aa969b628b450781e58151de18c
    Signed-off-by: NRanjan-17 <nalinishranjan05@gmail.com>
    Signed-off-by: Mr_ZerO <rbesar210311@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -52,6 +52,10 @@ endif
 PRODUCT_COPY_FILES += \
     vendor/afterlife/prebuilt/common/etc/init/init.afterlife-system_ext.rc:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/init/init.afterlife-system_ext.rc
 
+# Some permissions
+PRODUCT_COPY_FILES += \
+    vendor/afterlife/config/permissions/privapp-permissions-lineagehw.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-lineagehw.xml
+
 # Enable SIP+VoIP on all targets
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.software.sip.voip.xml
