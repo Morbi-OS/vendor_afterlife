ProjectBlaze/vendor_blaze

soong: Add TARGET_CAMERA_BOOTTIME_TIMESTAMP conditional
Change-Id: Ic48acda208fc9990fc5c9159ad06518d48f21477

config/BoardConfigSoong.mk
@@ -54,7 +54,8 @@ SOONG_CONFIG_blazeGlobalVars += \
     target_trust_usb_control_disable \
     uses_egl_display_array \
     uses_legacy_fd_fbdev \
-    uses_oplus_camera
+    uses_oplus_camera \
+    needs_camera_boottime
 
 SOONG_CONFIG_NAMESPACES += blazeNvidiaVars
 SOONG_CONFIG_blazeNvidiaVars += \
@@ -87,6 +88,7 @@ SOONG_CONFIG_blazeQcomVars_uses_pre_uplink_features_netmgrd := $(TARGET_USES_PRE
 SOONG_CONFIG_blazeGlobalVars_uses_legacy_fd_fbdev := $(TARGET_USES_LEGACY_FD_FBDEV)
 SOONG_CONFIG_blazeGlobalVars_uses_oplus_camera := $(TARGET_USES_OPLUS_CAMERA)
 SOONG_CONFIG_blazeGlobalVars_sdmcore_has_is_display_hw_available_func := $(TARGET_SDMCORE_HAS_IS_DISPLAY_HW_AVAILABLE_FUNC)
+SOONG_CONFIG_blazeGlobalVars_needs_camera_boottime := $(TARGET_CAMERA_BOOTTIME_TIMESTAMP)
 
 # Set default values
 BOOTLOADER_MESSAGE_OFFSET ?= 0

Morbi-OS/vendor_afterlife

Signed-off-by: Mr_ZerO <rbesar210311@gmail.com>

config/BoardConfigSoong.mk
@@ -57,7 +57,8 @@ SOONG_CONFIG_afterlifeGlobalVars += \
     target_trust_usb_control_disable \
     uses_egl_display_array \
     uses_legacy_fd_fbdev \
-    uses_oplus_camera
+    uses_oplus_camera \
+    needs_camera_boottime
 
 SOONG_CONFIG_NAMESPACES += afterlifeNvidiaVars
 SOONG_CONFIG_afterlifeNvidiaVars += \
@@ -92,6 +93,7 @@ SOONG_CONFIG_afterlifeQcomVars_uses_pre_uplink_features_netmgrd := $(TARGET_USES
 SOONG_CONFIG_afterlifeGlobalVars_uses_legacy_fd_fbdev := $(TARGET_USES_LEGACY_FD_FBDEV)
 SOONG_CONFIG_afterlifeGlobalVars_uses_oplus_camera := $(TARGET_USES_OPLUS_CAMERA)
 SOONG_CONFIG_afterlifeGlobalVars_sdmcore_has_is_display_hw_available_func := $(TARGET_SDMCORE_HAS_IS_DISPLAY_HW_AVAILABLE_FUNC)
+SOONG_CONFIG_afterlifeGlobalVars_needs_camera_boottime := $(TARGET_CAMERA_BOOTTIME_TIMESTAMP)
 
 # Set default values
 BOOTLOADER_MESSAGE_OFFSET ?= 0