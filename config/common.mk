The-PhoenixOS/vendor_phoenix

common: Add google dialer call recording feature
Change-Id: Ifc8ab79aa5bfee81774af96943d668074fc36ee5
Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>
Signed-off-by: afterallafk <shivamatiet2001@gmail.com>

config/common.mk
@@ -105,6 +105,10 @@ PRODUCT_PRODUCT_PROPERTIES += \
     ro.launcher.blur.appLaunch=0
 endif
 
+# Call Recording
+PRODUCT_COPY_FILES += \
+    vendor/flare/config/permissions/com.google.android.apps.dialer.call_recording_audio.features.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/com.google.android.apps.dialer.call_recording_audio.features.xml
+
 # Charger
 PRODUCT_PACKAGES += \
     charger_res_images \

Morbi-OS/vendor_afterlife

Signed-off-by: Mr_ZerO <rbesar210311@gmail.com>

config/common.mk
@@ -107,6 +107,10 @@ PRODUCT_PRODUCT_PROPERTIES += \
     ro.launcher.blur.appLaunch=0
 endif
 
+# Call Recording
+PRODUCT_COPY_FILES += \
+    vendor/afterlife/config/permissions/com.google.android.apps.dialer.call_recording_audio.features.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/com.google.android.apps.dialer.call_recording_audio.features.xml
+
 # Charger
 PRODUCT_PACKAGES += \
     charger_res_images \