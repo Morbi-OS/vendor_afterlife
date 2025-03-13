The-PhoenixOS/vendor_phoenix

vendor: Implement Lineage Health Service [3/3]
config: Copy Lineage health permissions

Change-Id: I12dcb124293cac0ba6f7275ee30498b711c723dd
Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>
Signed-off-by: afterallafk <shivamatiet2001@gmail.com>

config/common.mk
@@ -55,7 +55,8 @@ PRODUCT_COPY_FILES += \
 # Some permissions
 PRODUCT_COPY_FILES += \
     vendor/flare/config/permissions/privapp-permissions-lineagehw.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-lineagehw.xml \
-    vendor/flare/config/permissions/privapp-permissions-settings.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-settings.xml
+    vendor/flare/config/permissions/privapp-permissions-settings.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-settings.xml \
+    vendor/flare/config/permissions/org.lineageos.health.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/org.lineageos.health.xml
 
 # Enable Android Beam on all targets
 PRODUCT_COPY_FILES += \

Morbi-OS/vendor_afterlife

Signed-off-by: Mr_ZerO <rbesar210311@gmail.com>

config/common.mk
@@ -56,6 +56,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     vendor/afterlife/config/permissions/privapp-permissions-lineagehw.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-lineagehw.xml \
     vendor/afterlife/config/permissions/privapp-permissions-settings.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-settings.xml
+    vendor/afterlife/config/permissions/org.lineageos.health.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/org.lineageos.health.xml
 
 # Enable Android Beam on all targets
 PRODUCT_COPY_FILES += \