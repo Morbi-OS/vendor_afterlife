The-PhoenixOS/vendor_phoenix

vendor: Introduce App Lock [4/4]
Change-Id: Ie6d86e301efe14146c961b11bfccd3fa74a1562c

config/common.mk
@@ -54,7 +54,8 @@ PRODUCT_COPY_FILES += \
 
 # Some permissions
 PRODUCT_COPY_FILES += \
-    vendor/flare/config/permissions/privapp-permissions-lineagehw.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-lineagehw.xml
+    vendor/flare/config/permissions/privapp-permissions-lineagehw.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-lineagehw.xml \
+    vendor/flare/config/permissions/privapp-permissions-settings.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-settings.xml
 
 # Enable Android Beam on all targets
 PRODUCT_COPY_FILES += \

Morbi-OS/vendor_afterlife

Signed-off-by: Mr_ZerO <rbesar210311@gmail.com>

config/common.mk
@@ -54,7 +54,8 @@ PRODUCT_COPY_FILES += \
 
 # Some permissions
 PRODUCT_COPY_FILES += \
-    vendor/afterlife/config/permissions/privapp-permissions-lineagehw.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-lineagehw.xml
+    vendor/afterlife/config/permissions/privapp-permissions-lineagehw.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-lineagehw.xml \
+    vendor/afterlife/config/permissions/privapp-permissions-settings.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-settings.xml
 
 # Enable Android Beam on all targets
 PRODUCT_COPY_FILES += \