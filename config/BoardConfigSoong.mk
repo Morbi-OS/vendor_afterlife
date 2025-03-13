zaidkhan0997/vendor_blaze

soong: extend support for oppo camera
Change-Id: Ic24a503298587f55d0acff2381c98093c51daa87
Signed-off-by: S I P U N <sipunkumar85@gmail.com>

config/BoardConfigSoong.mk
@@ -59,7 +59,8 @@ SOONG_CONFIG_blazeGlobalVars += \
     target_camera_package_name \
     camera_needs_client_info \
     uses_nothing_camera \
-    uses_miui_camera
+    uses_miui_camera \
+    uses_oppo_camera
 
 SOONG_CONFIG_NAMESPACES += blazeNvidiaVars
 SOONG_CONFIG_blazeNvidiaVars += \
@@ -98,6 +99,7 @@ SOONG_CONFIG_blazeGlobalVars_camera_needs_client_info := $(TARGET_CAMERA_NEEDS_C
 SOONG_CONFIG_blazeGlobalVars_uses_nothing_camera := $(TARGET_USES_NOTHING_CAMERA)
 SOONG_CONFIG_blazeGlobalVars_camera_needs_client_info_lib_oplus := $(TARGET_CAMERA_NEEDS_CLIENT_INFO_LIB_OPLUS)
 SOONG_CONFIG_blazeGlobalVars_uses_miui_camera := $(TARGET_USES_MIUI_CAMERA)
+SOONG_CONFIG_blazeGlobalVars_uses_oppo_camera := $(TARGET_USES_OPPO_CAMERA)
 
 # Set default values
 BOOTLOADER_MESSAGE_OFFSET ?= 0

Morbi-OS/vendor_afterlife

Signed-off-by: Mr_ZerO <rbesar210311@gmail.com>

config/BoardConfigSoong.mk
@@ -62,7 +62,8 @@ SOONG_CONFIG_afterlifeGlobalVars += \
     target_camera_package_name \
     camera_needs_client_info \
     uses_nothing_camera \
-    uses_miui_camera
+    uses_miui_camera \
+    uses_oppo_camera
 
 SOONG_CONFIG_NAMESPACES += afterlifeNvidiaVars
 SOONG_CONFIG_afterlifeNvidiaVars += \
@@ -103,6 +104,7 @@ SOONG_CONFIG_afterlifeGlobalVars_camera_needs_client_info := $(TARGET_CAMERA_NEE
 SOONG_CONFIG_afterlifeGlobalVars_uses_nothing_camera := $(TARGET_USES_NOTHING_CAMERA)
 SOONG_CONFIG_afterlifeGlobalVars_camera_needs_client_info_lib_oplus := $(TARGET_CAMERA_NEEDS_CLIENT_INFO_LIB_OPLUS)
 SOONG_CONFIG_afterlifeGlobalVars_uses_miui_camera := $(TARGET_USES_MIUI_CAMERA)
+SOONG_CONFIG_afterlifeGlobalVars_uses_oppo_camera := $(TARGET_USES_OPPO_CAMERA)
 
 # Set default values
 BOOTLOADER_MESSAGE_OFFSET ?= 0